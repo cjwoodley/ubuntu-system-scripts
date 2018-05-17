e8c89dbef335221f445395a13a77e2a7541eca7a
C.J. Woodley <cecil.woodley@gmail.com>
2018-05-16 22:35:11 -0500

    inital for linux powershell core

---
+++ installDotNetCore.sh
@@ -0,0 +1,11 @@
+# Import the public repository GPG keys
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+# Register the Microsoft Ubuntu repository
+sudo curl -o /etc/apt/sources.list.d/microsoft.list https://packages.microsoft.com/config/ubuntu/17.04/prod.list
+
+# Update the list of products
+sudo apt-get update
+
+# Install PowerShell
+sudo apt-get install -y powershell
